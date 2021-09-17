2abc43f49560bf22891964604cb263e8dd5702ff
neutralmike <neutralmike@yandex.ru>
2021-09-17 13:20:48 +0300

    frontend trial A complited

---
+++ frontend/trial/A/A.js
@@ -0,0 +1,29 @@
+function getInsertInd(weights, matter){
+  if (weights.length === 1){
+    return + (matter > weights[0])
+  }
+  const mid = Math.floor(weights.length / 2)
+  if (matter < weights[mid]){
+    return getInsertInd(weights.slice(0, mid), matter)
+  }
+  return mid + getInsertInd(weights.slice(mid), matter)
+  
+}
+
+function findLatestWeight (weights) {  
+  weights.sort((a, b) => a - b)
+  while (weights.length > 1)  {
+    matter = Math.abs(weights.pop() - weights.pop())
+    if (weights.length === 0){
+      return matter
+    }
+    if (matter > 0){
+      weights.splice(getInsertInd(weights, matter) , 0, matter)
+    }
+  }
+  return weights[0]
+}
+
+
+module.exports = findLatestWeight; 
+console.log(findLatestWeight([2,7,4,1,8,1]))
