e3fd407f2ed60fe65994e50aa27ee0b8bf717f84
neutralmike <neutralmike@yandex.ru>
2021-09-25 09:46:30 +0300

    backend trial21 C

--- backend/trial21/C/test.py
+++ backend/trial21/C/test.py
@@ -1,15 +1,14 @@
 from http.server import SimpleHTTPRequestHandler, HTTPServer
-import socketserver
 import json
 
 PORT = 7777
+
 
 class Server(SimpleHTTPRequestHandler):
     def _set_headers(self):
         self.send_response(200)
         self.send_header('Content-type', 'application/json')
         self.end_headers()
-
 
     def do_GET(self):
         self._set_headers()
